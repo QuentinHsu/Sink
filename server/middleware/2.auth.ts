64ca54c32529d85b35957be858cd43293f62d08c
QuentinHsu <xuquentinyang@gmail.com>
2024-09-02 10:24:36 +0800

    perf: force all tokens to be converted to strings for comparison

--- server/middleware/2.auth.ts
+++ server/middleware/2.auth.ts
@@ -1,6 +1,7 @@
 export default eventHandler((event) => {
   const token = getHeader(event, 'Authorization')?.replace('Bearer ', '')
-  if (event.path.startsWith('/api/') && !event.path.startsWith('/api/_') && token !== useRuntimeConfig(event).siteToken) {
+  const correctToken = String(useRuntimeConfig(event).siteToken)
+  if (event.path.startsWith('/api/') && !event.path.startsWith('/api/_') && token !== correctToken) {
     throw createError({
       status: 401,
       statusText: 'Unauthorized',
